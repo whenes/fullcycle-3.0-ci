49c0e7951383093e61d41be2671a9a1b7c2dd459
whenes <whenes.oliveira@gmail.com>
2022-03-01 19:59:52 -0300

    fix: Fixed math_test erros

--- math_test.go
+++ math_test.go
@@ -1,7 +1,9 @@
 package main
 
+import "testing"
+
 func TestSoma(t *testing.T) {
-	total := soma(15, 15)
+	total := Soma(15, 15)
 	if (total != 30) {
 		t.Errorf("Resultado da soma é inválido! Resultado %d. Esperado %d.", total, 30)
 	}
