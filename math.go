b4071f2115b8c00c0bb6fcf3a8120fa8dabfeb3c
whenes <whenes.oliveira@gmail.com>
2022-03-01 19:39:56 -0300

    feat: :sparkles: Adicionada funçao de soma

---
+++ math.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	fmt.Println(Soma(10, 10))
+}
+
+func Soma(a int, b int) int {
+	return a + b
+}
---
+++ math_test.go
@@ -0,0 +1,8 @@
+package main
+
+func TestSoma(t *testing.T) {
+	total := soma(15, 15)
+	if (total != 30) {
+		t.Errorf("Resultado da soma é inválido! Resultado %d. Esperado %d.", total, 30)
+	}
+}
